ae0c59bbd03fe00cb2e12f17ab4a349f6d918810
Werk Diogo Gonçalves <coelho.diogogoncalves@gmail.com>
2023-05-23 12:24:47 +0100

    Formulário criado com estilos

--- index.php
+++ index.php
@@ -21,10 +21,29 @@ echo "Connected successfully";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
 </head>
 <body>
-    <h1>test</h1>
-    
+    <h1 id="title">Formulário</h1>
+        <div class="master-div-style">
+            <p id="description">Preencha todos os campos do Formulário.</p>
+            <form id="survey-form" action="mailto:coelho.diogogoncalves@gmail.com">
+                <div class="form-rows">
+                    <div class="labels">
+                        <label for="name" id="name-label">Nome</label>
+                    </div>
+                    <div class="fields">
+                        <input type="text" id="name" class="input-fields" placeholder="Escreva o seu nome completo"
+                            required>
+                    </div>
+                </div>
+                
+                </div>
+                <button class="button" id="submit" type="submit">Submit</button>
+                <button class="button" id="submit" type="reset">Reset</button>
+            </form>
+        </div>
+    </main>
 </body>
 </html>
 </html>
