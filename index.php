cbb3e2537bd1549dd3ca387f8c977389971b468b
Werk Diogo Gonçalves <coelho.diogogoncalves@gmail.com>
2023-05-23 15:46:55 +0100

    index.php com apenas uma tabela com um select a funcionar

--- index.php
+++ index.php
@@ -1,19 +1,46 @@
 <?php
+require 'funcoes.php';
+
 $servername = "localhost";
 $username = "root";
 $password = "usbw";
+$db_name = "crud";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $db_name);
 
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  die(debug("Connection failed: " . $conn->connect_error));
 }
-echo "Connected successfully";
+
+debug("Connected successfully");
+
+$sql = "SELECT * FROM `test` where ativo = 1";
+
+$result = mysqli_query($conn, $sql);
+
+echo "<table border='1'>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+echo "<tr>";
+echo "<td>" . $row['nome'] . "</td>";
+echo "<td>" . $row['ativo'] . "</td>";
+echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+
+
 ?>
 
-<<html>
+<html>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,7 +50,17 @@ echo "Connected successfully";
     <title>Document</title>
     <link rel="stylesheet" type="text/css" href="styles/styles.css">
 </head>
+
 <body>
+        <!--Navbar-->
+    <div class="navbar" id="navbar">
+        <a class="active" href="#home">Home</a>
+        <a href="">Futebol</a>
+        <a href="#basketball">Basketball</a>
+        <a href="#tenis">Tenis</a>
+        <a href="#remos">Remos</a>
+    </div>
+
     <h1 id="title">Formulário</h1>
         <div class="master-div-style">
             <p id="description">Preencha todos os campos do Formulário.</p>
