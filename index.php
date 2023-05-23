984470dcf3ab1c9d08d6cfe887e85edbec9b8a35
Werk Diogo Gonçalves <coelho.diogogoncalves@gmail.com>
2023-05-23 12:16:00 +0100

    Introduzir o php dentro de um ficheiro HTML, rereduzido a um ficheiro só´

--- index.php
+++ index.php
@@ -12,3 +12,19 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 ?>
+
+<<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>test</h1>
+    
+</body>
+</html>
+</html>
